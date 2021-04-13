2cb048b15a4f7652ebddec5e9a4f25be74d298d5
SuwenShaw <xiaoshuwen@163.com>
2021-04-13 18:31:07 +0800

    update:更新README

---
+++ analysis/sql/用户产品偏好分析.sql
@@ -0,0 +1,48 @@
+-- 用户产品偏好分析
+-- 访问量前十的产品，以及它们的访问量、收藏加购率、购买率
+select sku_code,
+       pv,
+       concat(round((fav + cart) / pv * 100, 2), '%') as pv_to_favcart,
+       concat(round(buy / pv * 100, 2), '%')          as pv_to_buy
+from (
+         select sku_code, count(url) as pv
+         from (
+                  select sku_code, url
+                  from web_action_log
+                  union all
+                  select sku_code, url
+                  from app_action_log
+              ) as temp
+         group by sku_code
+     ) as t1
+         left join (
+    select sku_code,
+           sum(case when action_type = 'fav' then 1 else 0 end)  as fav,
+           sum(case when action_type = 'cart' then 1 else 0 end) as cart
+    from behavior_log
+    group by sku_code
+) as t2
+                   on t1.sku_code = t2.sku_code
+         left join (
+    select sku_code,
+           count(order_detail_no) as buy
+    from dim_order_info as m_order
+             inner join fact_order_detail_info as detail
+                        on detail.order_no = m_order.order_no
+    group by sku_code
+) as t3
+                   on t1.sku_code = t3.sku_code
+order by pv desc
+limit 10
+;
+
+-- 销量前十的产品
+select sku_code,
+       count(order_detail_no) as buy
+from dim_order_info as m_order
+         inner join fact_order_detail_info as detail
+                    on detail.order_no = m_order.order_no
+group by sku_code
+order by buy desc
+limit 10
+;
---
+++ analysis/sql/用户行为漏斗分析.sql
@@ -0,0 +1,189 @@
+-- 用户行为漏斗分析
+-- PV（访问量）
+select count(url) as 总访问量
+from (
+         select url
+         from web_action_log
+         union all
+         select url
+         from app_action_log
+     ) as temp
+;
+
+-- fav（收藏）、cart（加购物车）
+select sum(case when action_type='fav' then 1 else 0 end) as 总收藏量,
+       sum(case when action_type='cart' then 1 else 0 end) as 总加购量
+       from behavior_log
+;
+
+-- buy（购买）
+select count(order_detail_no) as 总购买量
+from dim_order_info as m_order
+         inner join fact_order_detail_info as detail
+                    on detail.order_no = m_order.order_no
+;
+
+-- 用户总访问、收藏、加购、购买量
+-- method1
+select sum_pv,sum_fav,sum_cart,sum_buy
+from (
+         (
+             select count(url) as sum_pv,null as sum_fav,null as sum_cart,null as sum_buy
+             from (
+                      select url
+                      from web_action_log
+                      union all
+                      select url
+                      from app_action_log
+                  ) as temp
+         )
+         union all
+         (select null as sum_pv,
+                 sum(case when action_type = 'fav' then 1 else 0 end) as sum_fav,
+                 null as sum_cart,null as sum_buy
+          from behavior_log
+          )
+         union all
+         (select null as sum_pv,null as sum_fav,
+                 sum(case when action_type = 'cart' then 1 else 0 end) as sum_cart,
+                 null as sum_buy
+          from behavior_log
+          )
+         union all
+         (select null as sum_pv,null as sum_fav,null as sum_cart,
+                 count(order_detail_no) as sum_buy
+          from dim_order_info as m_order
+                   inner join fact_order_detail_info as detail
+                              on detail.order_no = m_order.order_no
+          )
+     ) as temp
+;
+
+-- method2
+select sum(pv) as sum_pv, sum(fav) as sum_fav, sum(cart) as sum_cart, sum(buy) as sum_buy
+from (
+         select user_id, count(url) as pv
+         from (
+                  select user_id, url
+                  from web_action_log
+                  union all
+                  select user_id, url
+                  from app_action_log
+              ) as temp
+         group by user_id
+     ) as t1
+         left join (
+    select user_id,
+           sum(case when action_type = 'fav' then 1 else 0 end)  as fav,
+           sum(case when action_type = 'cart' then 1 else 0 end) as cart
+    from behavior_log
+    group by user_id
+) as t2
+                   on t1.user_id = t2.user_id
+         left join (
+    select person_id,
+           count(order_detail_no) as buy
+    from dim_order_info as m_order
+             inner join fact_order_detail_info as detail
+                        on detail.order_no = m_order.order_no
+    group by person_id
+) as t3
+                   on t1.user_id = t3.person_id
+;
+
+-- method3
+select sum_pv, sum_fav, sum_cart, sum_buy
+from (
+         select 'key' as t_key, count(url) as sum_pv
+         from (
+                  select url
+                  from web_action_log
+                  union all
+                  select url
+                  from app_action_log
+              ) as temp
+     ) as t1
+         left join (
+    select 'key'                                                 as t_key,
+           sum(case when action_type = 'fav' then 1 else 0 end)  as sum_fav,
+           sum(case when action_type = 'cart' then 1 else 0 end) as sum_cart
+    from behavior_log
+) as t2
+                   on t1.t_key = t2.t_key
+         left join (
+    select 'key'                  as t_key,
+           count(order_detail_no) as sum_buy
+    from dim_order_info as m_order
+             inner join fact_order_detail_info as detail
+                        on detail.order_no = m_order.order_no
+) as t3
+                   on t1.t_key = t3.t_key
+;
+
+-- 用户各行为转化率
+-- method1
+select concat(round(sum_pv / sum_pv * 100, 2), '%')                as pv_to_pv,
+       concat(round((sum_fav + sum_cart) / sum_pv * 100, 2), '%')  as pv_to_favcart,
+       concat(round(sum_buy / (sum_fav + sum_cart) * 100, 2), '%') as favcart_to_buy
+from (
+         select sum_pv, sum_fav, sum_cart, sum_buy
+         from (
+                  select 'key' as t_key, count(url) as sum_pv
+                  from (
+                           select url
+                           from web_action_log
+                           union all
+                           select url
+                           from app_action_log
+                       ) as temp
+              ) as t1
+                  left join (
+             select 'key'                                                 as t_key,
+                    sum(case when action_type = 'fav' then 1 else 0 end)  as sum_fav,
+                    sum(case when action_type = 'cart' then 1 else 0 end) as sum_cart
+             from behavior_log
+         ) as t2
+                            on t1.t_key = t2.t_key
+                  left join (
+             select 'key'                  as t_key,
+                    count(order_detail_no) as sum_buy
+             from dim_order_info as m_order
+                      inner join fact_order_detail_info as detail
+                                 on detail.order_no = m_order.order_no
+         ) as t3
+                            on t1.t_key = t3.t_key
+     ) as temp
+;
+
+-- method2
+with t1
+         as (select 'key'      as t_key,
+                    count(url) as sum_pv
+             from (
+                      select url
+                      from web_action_log
+                      union all
+                      select url
+                      from app_action_log
+                  ) as temp
+    ),
+     t2
+         as (select 'key'                                                 as t_key,
+                    sum(case when action_type = 'fav' then 1 else 0 end)  as sum_fav,
+                    sum(case when action_type = 'cart' then 1 else 0 end) as sum_cart
+             from behavior_log),
+     t3
+         as (select 'key'                  as t_key,
+                    count(order_detail_no) as sum_buy
+             from dim_order_info as m_order
+                      inner join fact_order_detail_info as detail
+                                 on detail.order_no = m_order.order_no)
+select concat(round(sum_pv / sum_pv * 100, 2), '%')                as pv_to_pv,
+       concat(round((sum_fav + sum_cart) / sum_pv * 100, 2), '%')  as pv_to_favcart,
+       concat(round(sum_buy / (sum_fav + sum_cart) * 100, 2), '%') as favcart_to_buy
+from t1
+         inner join t2
+                    on t1.t_key = t2.t_key
+         inner join t3
+                    on t1.t_key = t3.t_key
+;
